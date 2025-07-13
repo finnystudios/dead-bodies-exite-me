5a33ed63d08006dc41ab3ca92ae60204b6b7d27a
Sam <140025368+sam0622@users.noreply.github.com>
2025-07-12 23:32:15 -0400

    Added interactable detection and prompts

---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace Interactables
+{
+    [RequireComponent(typeof(Outline))]
+    public class Interactable : MonoBehaviour
+    {
+        [SerializeField] private bool isInteractable = true;
+        public string prompt;
+
+        private Outline _outline;
+
+        public bool IsInteractable => isInteractable;
+
+        private void Awake()
+        {
+            _outline = GetComponent<Outline>();
+            _outline.enabled = false;
+        }
+
+        public virtual void Interact()
+        {
+            Debug.Log($"Player interacted with {name}");
+        }
+
+        public void EnableOutline()
+        {
+            _outline.enabled = true;
+        }
+
+        public void DisableOutline()
+        {
+            _outline.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInteractionController.cs
@@ -0,0 +1,69 @@
+using Interactables;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    public class PlayerInteractionController : MonoBehaviour
+    {
+        [SerializeField] private Camera playerCamera;
+        [SerializeField] private float interactionRange = 3f;
+        
+        private Interactable _currentInteractable;
+        private Interactable CurrentInteractable
+        {
+            get => _currentInteractable;
+            set
+            {
+                _currentInteractable = value;
+                _interactionPrompt.text = value ? value.prompt : string.Empty;
+            }
+        }
+        
+        private TextMeshProUGUI _interactionPrompt;
+        
+        private void Awake()
+        {
+            _interactionPrompt = GetComponentInChildren<HUDController>().InteractionPrompt;
+        }
+        
+        private void Update()
+        {
+            // ReSharper disable PossibleLossOfFraction
+            var ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            // ReSharper restore PossibleLossOfFraction
+
+            // If nothing is in interaction range, set current interactable to null and return early
+            // The out keyword lets a function return multiple separate values and makes the returned value a variable,
+            // see https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/out for more info
+            if (!Physics.Raycast(ray, out var hit, interactionRange))
+            {
+                CurrentInteractable?.DisableOutline();
+                CurrentInteractable = null;
+                return;
+            }
+
+            // If hit isn't interactable, return
+            if (!hit.transform.TryGetComponent(out Interactable interactable) || !interactable.IsInteractable) return;
+
+            if (interactable == CurrentInteractable) return;
+
+            if (interactable.IsInteractable)
+            {
+                CurrentInteractable = interactable;
+                CurrentInteractable.EnableOutline();
+            }
+            else
+            {
+                CurrentInteractable = null;
+            }
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            if (!context.performed) return;
+            CurrentInteractable?.Interact();
+        }
+    }
+}
